ce6b4c3464ad4e78882732b02e432876c8c8612a
Ohad Stoller <ohadstoller@Ohads-MacBook-Pro.local>
2022-01-13 11:31:41 +0200

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import ComboBox from '../components/ComboBox'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <ComboBox/>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
